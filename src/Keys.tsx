c961c744d733adbf4c32ce78d24a7f1d255e4f5f
Горюнов Иван <ShioSR@yandex.ru>
2025-05-19 01:33:05 +0300

    Ready

--- src/Keys.tsx
+++ src/Keys.tsx
@@ -31,7 +31,7 @@ export function Keys(props: KeysProps) {
 
         sortData();
         console.log(sorting);
-    }, [sorting]);
+    }, [currentSorting, props.initialData]);
 
     const handleNameClick = (id: number, name: string) => {
         setEditingId(id);
